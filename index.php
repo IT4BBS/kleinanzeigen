ac1a8840a7c98165cc868a6146ec1a5a0c719ac2
athamm1 <a.thamm1@gmx.net>
2016-10-31 10:35:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 
+<head>
+    <title>BBS Kleinanzeigen</title>
+
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="" />
+    <meta name="generator" content="Webocton - Scriptly (www.scriptly.de)" />
+
+    <link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+   <div id="header_1">
+       <h1>BBS Kleinanzeigen</h1>
+   </div>
+     <div id="menu1">
+       <a href="#">Home</a>            &nbsp;&nbsp;
+       <a href="#">Registrieren</a>    &nbsp;&nbsp;
+       <a href="#">Hilfe</a>           &nbsp;&nbsp;
+   </div>
+    <div id="content_bg">
+       
+   </div>
+   <div id="search">
+        <form>
+            <label for="suche">
+                        <input id="suche" name="suche"> 
+                        <input type="submit" value="Suchen">
+            </label>
+      </form>
+   </div>
+   <div id="category">
+       <h3>Kategorien:</h3>
+       ----------------<br />
+       <a href="/kategorie/hifi">Audio und Hifi</a><br />
+       <a href="/kategorie/auto">Auto und Motorrad</a> <br />
+       <a href="/kategorie/buecher">B&uuml;cher</a> <br />
+       <a href="/kategorie/buero">B&uuml;ro und Schreibwaren</a> <br />
+       <a href="/kategorie/elektro">Computer, <br />Handy,Netzwerk</a> 
+       <a href="/kategorie/elektronik">Elektronik</a> <br />
+       <a href="/kategorie/dvd">Filme und DVD</a> <br />
+       <a href="/kategorie/kamera">Kameras</a> <br />
+   </div>
+</body>
+</html>
